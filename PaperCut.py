b4da4f55298cc97446380ef24348772d7d9ba051
Shmupo <andrewdoan8b@gmail.com>
2022-10-01 11:04:30 -0700

    Images and code for the game

--- PaperCut.py
+++ PaperCut.py
@@ -8,18 +8,21 @@ class Game:
     def __init__(self):
         pg.init()
         self.settings = Settings()
-        size = self.settings.window_x, self.settings.window_y
-        self.screen = pg.display.set_mode(size=size)
+        self.size = self.settings.window_x, self.settings.window_y
+        self.screen = pg.display.set_mode(size=self.size)
         pg.display.set_caption("Papercut")
+
+        background = pg.image.load('images/background.png')
+        self.background = pg.transform.scale(background, self.size)
 
     # this is the video game loop where everything should be updated
     def play(self):
         while True:
-            self.screen.fill((0 ,0 ,0))
+            self.screen.blit(self.background, (0, 0))
 
             # there is an image that is off-screen but is still loaded and
             # needs to be switched with the current screen
-            pg.display.flip()
+            pg.display.update()
 
 
 
---
+++ card_base.py
@@ -0,0 +1,8 @@
+#This modules should only be used for testing is is used as a parent class for other card classes
+
+class Card:
+    def __init__(self, name='Base Card', description='This is a base card', card_image):
+        self.name = name
+        self.description = description
+
+        
