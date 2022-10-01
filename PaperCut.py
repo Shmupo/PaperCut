6d39cb08c00978021e0d1ab96cac96e230933a6a
Shmupo <andrewdoan8b@gmail.com>
2022-10-01 11:33:48 -0700

    Included helpful resources.

--- PaperCut.py
+++ PaperCut.py
@@ -2,6 +2,7 @@
 
 import pygame as pg
 from settings import Settings
+from card_base import Card
 
 # The entirety of the game will mostly run here
 class Game:
@@ -14,6 +15,9 @@ class Game:
 
         background = pg.image.load('images/background.png')
         self.background = pg.transform.scale(background, self.size)
+        
+        card_image = pg.image.load('images/playercard.png')
+        self.test_card = Card(self, card_image)
 
     # this is the video game loop where everything should be updated
     def play(self):
@@ -22,6 +26,7 @@ class Game:
 
             # there is an image that is off-screen but is still loaded and
             # needs to be switched with the current screen
+            self.test_card.update()
             pg.display.update()
 
 
--- card_base.py
+++ card_base.py
@@ -1,8 +1,32 @@
 #This modules should only be used for testing is is used as a parent class for other card classes
+import pygame as pg
 
 class Card:
-    def __init__(self, name='Base Card', description='This is a base card', card_image):
+    def __init__(self, game, card_image, name='Base Card', description='This is a base card'):
+        self.game = game
+        self.screen = self.game.screen
         self.name = name
         self.description = description
+        self.image = card_image
+        # a list of what the card can interact with
+        self.targets = []
+        self.pos_x = 0
+        self.pos_y = 0
 
-        
+        background = pg.image.load('images/background.png')
+
+    # moves the card when the user drags it around
+    def check_drag(self):
+        pass
+
+    # checks if the card was dropped on a target and if it is valid
+    def check_target(self):
+        pass
+
+    def draw(self):
+        self.screen.blit(self.image, (self.pos_x, self.pos_y))
+
+    def update(self):
+        self.check_drag()
+        self.check_target()
+        self.draw()
